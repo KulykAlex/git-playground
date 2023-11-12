6390d4f366324dc922a070e9df837d49e8898175
KulykAlex <118806934+KulykAlex@users.noreply.github.com>
2023-11-12 15:59:32 +0200

    if player loses

--- main.py
+++ main.py
@@ -60,3 +60,6 @@ while not is_game_over():
     else:
         errors += 1
         print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives more")
+
+if errors == ERRORS_TO_LOSE:
+    print("You lose")
